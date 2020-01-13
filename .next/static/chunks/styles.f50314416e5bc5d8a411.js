bb799ead39da5bc88bc4124cf12dca202dbc25aa
Jeremi Proulx <exnxs@hotmail.com>
2020-01-12 19:40:39 -0500

    First commit

---
+++ .next/static/chunks/styles.f50314416e5bc5d8a411.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"00et":function(n,w,o){}}]);
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+
+const Index = (props) => {
+    const myname = useRef(null);
+    const myimage = useRef(null);
+
+    const effetDependencies = [myname,myimage];
+
+    useEffect(() => {
+        gsap.from(myname.current, 1, {x: -100});
+        gsap.from(myimage.current, 1, {y: 300});
+    }, effetDependencies);
+
+
+    return(
+        <div >
+            <p ref={myname}>My name is {props.name}</p>
+            <img ref={myimage} src="/logo.png" alt="my image" />
+        </div>
+    )
+}
+
+
+export default Index;
